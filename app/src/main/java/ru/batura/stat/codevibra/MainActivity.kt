aa45af21db6b027c52087df6a932c85abcbc9f6a
home <seeyou85@gmail.com>
2020-02-24 21:30:03 +0300

    first commit

---
+++ app/src/main/java/ru/batura/stat/codevibra/MainActivity.kt
@@ -0,0 +1,19 @@
+package ru.batura.stat.codevibra
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.batura.stat.codevibra.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/ru/batura/stat/codevibra/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package ru.batura.stat.codevibra.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
